1afb38adbdee541bcab75bf62bbdcaa569c4b6f5
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2022-09-20 13:39:00 +1000

    Project Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Image src={"/images/starClipsLogo.png"} height={"700"} width={"1000"} />
+      <h1>Index</h1>
     </>
   );
 };
